befd65baeef1b2347f8eee149b4373116bd0759b
4MBL <74970679+4MBL@users.noreply.github.com>
2023-05-23 13:55:32 +0300

    feat: return string instead of printing the syntax highlighted string

--- src/fortext/pretty_print.py
+++ src/fortext/pretty_print.py
@@ -22,13 +22,13 @@ def syntax_highlight_string(val, colors=None):
     return repr(val)
 
 
-def pretty_print_dict(dictionary: dict,
-                      prettify_lists=False,
-                      indent: int = 2,
-                      curr_indent: int = 0,
-                      trailing_comma=False,
-                      do_pre_indent=True,
-                      colors=None):
+def pretty_dict(dictionary: dict,
+                prettify_lists=False,
+                indent: int = 2,
+                curr_indent: int = 0,
+                trailing_comma=False,
+                do_pre_indent=True,
+                colors=None):
     if colors is None:
         colors = DEFAULT_SYNTAX_HIGHLIGHT_COLORS
 
@@ -36,47 +36,47 @@ def pretty_print_dict(dictionary: dict,
     rcub = style('}', fg=colors['dict'])
 
     pre_identation = " " * curr_indent if do_pre_indent else ''
-    print(f'{pre_identation}{lcub}')
+    output_str = f'{pre_identation}{lcub}\n'
 
     for i, (key, val) in enumerate(dictionary.items()):
 
         pretty_key = style(repr(key), fg=colors['key'])
 
         if isinstance(val, dict):
-            print(f'{" "*(curr_indent + indent)}{pretty_key}: ', end='')
-            pretty_print_dict(val,
-                              indent=curr_indent + indent,
-                              curr_indent=curr_indent + indent,
-                              trailing_comma=True,
-                              do_pre_indent=False)
+            output_str += f'{" "*(curr_indent + indent)}{pretty_key}: '
+            output_str += pretty_dict(val,
+                                      indent=curr_indent + indent,
+                                      curr_indent=curr_indent + indent,
+                                      trailing_comma=True,
+                                      do_pre_indent=False)
             continue
         if isinstance(val, list) and prettify_lists:
-            print(f'{" "*(curr_indent + indent)}{pretty_key}: ', end='')
-            pretty_print_list(val)
+            output_str += f'{" "*(curr_indent + indent)}{pretty_key}: '
+            output_str += f'{pretty_list(val)}\n'
             continue
 
         comma = ',' if (i < len(dictionary) - 1) else ''
-
         pretty_value = syntax_highlight_string(val)
 
-        print(
-            f'{" "*(curr_indent + indent)}{pretty_key}: {pretty_value}{comma}')
+        output_str += f'{" "*(curr_indent + indent)}{pretty_key}: {pretty_value}{comma}\n'
 
-    print(f'{" "* curr_indent}{rcub}{"," if trailing_comma else ""}')
+    output_str += f'{" "* curr_indent}{rcub}{"," if trailing_comma else ""}\n'
+    return output_str
 
 
-def pretty_print_list(lst: list, trailing_comma=False, colors=None):
+def pretty_list(lst: list, trailing_comma=False, colors=None):
     if colors is None:
         colors = DEFAULT_SYNTAX_HIGHLIGHT_COLORS
 
     lsqb = style('[', fg=colors['arr'])
     rsqb = style(']', fg=colors['arr'])
 
-    print(f'{lsqb}', end='')
+    output_str = f'{lsqb}'
 
     for i, val in enumerate(lst):
         comma = ', ' if (i < len(lst) - 1) else ''
         pretty_value = syntax_highlight_string(val)
-        print(f'{pretty_value}{comma}', end='')
+        output_str += f'{pretty_value}{comma}'
 
-    print(f'{rsqb}{"," if trailing_comma else ""}')
+    output_str += f'{rsqb}{"," if trailing_comma else ""}'
+    return output_str
