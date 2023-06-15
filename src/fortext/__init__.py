d64b75be2fdbdba3e8a12c11990548ce1491a97c
4MBL <74970679+4MBL@users.noreply.github.com>
2023-06-15 17:44:58 +0300

    feat: make `print_styles` and `print_styles_all` available to import

--- src/fortext/__init__.py
+++ src/fortext/__init__.py
@@ -1,3 +1,3 @@
-from .ansi import Fg, Bg, Frmt
+from .ansi import Fg, Bg, Frmt, print_styles, print_styles_all
 from .style import style
 from .syntax import highlight, DEFAULT_SYNTAX_HIGHLIGHT_COLORS
